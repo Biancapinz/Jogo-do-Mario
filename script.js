ab825d17ccce5ccf394637e5803b75b50a40cf52
Biancapinz <133228830+Biancapinz@users.noreply.github.com>
2023-06-07 12:33:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const mario = document.querySelector('.mario')
-
-const jump {
-    mario.classList.add('jump')
+// cria o elemento
+const mario = document.querySelector('.mario');
+// faz o jump
+const jump = () => {
+    mario.classList.add('jump');
 }
-document addEventListener('keydown', jump)
+document.addEventListener('keydown', jump);
