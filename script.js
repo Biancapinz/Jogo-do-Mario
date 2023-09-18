3b430fb811753c188964a5f97883d24722c23988
lorenzozardo <133228830+Biancapinz@users.noreply.github.com>
2023-09-18 14:44:23 -0300

    teste

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const jump = () => {
     mario.classList.add('jump');
 }
 document.addEventListener('keydown', jump);
+i=0;
+while (i <= 20) {
+    text = "The number is " + i;
+    
+    console.log( text);
+
+    i++;
+}
